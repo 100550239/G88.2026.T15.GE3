81d870dec079412666939722c3d515e4a9236f79
Alberto Martin <100550271@alumnos.uc3m.es>
2026-04-24 12:11:11 +0200

    Design: add Singleton to ProjectJsonStore

--- src/main/python/uc3m_consulting/project_json_store.py
+++ src/main/python/uc3m_consulting/project_json_store.py
@@ -6,6 +6,15 @@ from uc3m_consulting.json_store import JsonStore
 class ProjectJsonStore:
     """Class responsible for project data persistence"""
 
+    __instance = None
+
+    def __new__(cls):
+        """Ensures a single instance for project storage"""
+        if cls.__instance is None:
+            cls.__instance = super(ProjectJsonStore, cls).__new__(cls)
+        return cls.__instance
+
+
     @staticmethod
     def add_project(project_json: dict):
         """Adds a new project to the JSON file verifying duplicates"""
