e162a69cbfb761562a13b5b0d2706054ed1bd4cc
100550239 <100550239@alumnos.uc3m.es>
2026-04-24 12:20:22 +0200

    Minimal changes to make it pass pylint test

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -3,8 +3,7 @@ from datetime import datetime, timezone
 from freezegun import freeze_time
 from uc3m_consulting.enterprise_project import EnterpriseProject
 from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
-from uc3m_consulting.enterprise_manager_config import (PROJECTS_STORE_FILE,
-                                                       TEST_DOCUMENTS_STORE_FILE,
+from uc3m_consulting.enterprise_manager_config import (TEST_DOCUMENTS_STORE_FILE,
                                                        TEST_NUMDOCS_STORE_FILE)
 from uc3m_consulting.project_document import ProjectDocument
 from uc3m_consulting.json_store import JsonStore
@@ -101,4 +100,3 @@ class EnterpriseManager:
         self._save_search_report(date_str, documents_found)
 
         return documents_found
-
